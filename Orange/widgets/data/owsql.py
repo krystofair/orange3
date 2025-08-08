472aa24a384e66b8d3a5a9ba40f9de032c5d0c32
Krzysztof Kłapyta <33362868+krystofair@users.noreply.github.com>
2025-08-08 22:06:45 +0200

    Mods to SQLTable widget. Can loads table from file
    Created for user to have more freedom to keep own's SELECTs,
    because user may not have permission to create materialized views.
    
    Loading tables from file specified by path. File with structure:
    -- name: <name visible as table name>
    SELECT ... ;
    -- name: <another>
    select ... ;

--- Orange/widgets/data/owsql.py
+++ Orange/widgets/data/owsql.py
@@ -1,9 +1,6 @@
-from AnyQt.QtWidgets import QComboBox, QTextEdit, QMessageBox, QApplication, \
-    QGridLayout, QLineEdit
+from AnyQt.QtWidgets import QComboBox, QTextEdit, QMessageBox, QApplication
 from AnyQt.QtGui import QCursor
 from AnyQt.QtCore import Qt
-
-from orangewidget.utils.combobox import ComboBoxSearch
 
 from Orange.data import Table
 from Orange.data.sql.backend import Backend
@@ -17,7 +14,6 @@ from Orange.widgets.utils.widgetpreview import WidgetPreview
 from Orange.widgets.widget import Output, Msg
 
 MAX_DL_LIMIT = 1000000
-MAX_TABLES = 1000
 
 
 def is_postgres(backend):
@@ -56,12 +52,13 @@ class OWSql(OWBaseSql):
 
     buttons_area_orientation = None
 
-    TABLE, CUSTOM_SQL = range(2)
     selected_backend = Setting(None)
-    data_source = Setting(TABLE)
     table = Setting(None)
     sql = Setting("")
     guess_values = Setting(True)
+    download = Setting(False)
+
+    path_to_res_file = Setting("")
 
     materialize = Setting(False)
     materialize_table_name = Setting("")
@@ -69,8 +66,13 @@ class OWSql(OWBaseSql):
     class Information(OWBaseSql.Information):
         data_sampled = Msg("Data description was generated from a sample.")
 
+    class Warning(OWBaseSql.Warning):
+        missing_extension = Msg("Database is missing extensions: {}")
+
     class Error(OWBaseSql.Error):
         no_backends = Msg("Please install a backend to use this widget.")
+        file_os_error = Msg("File doesn't exist or you don't have read permission.")
+        empty_path = Msg("Path cannot be empty.")
 
     def __init__(self):
         # Lint
@@ -78,15 +80,74 @@ class OWSql(OWBaseSql):
         self.backendcombo = None
         self.tables = None
         self.tablecombo = None
-        self.tabletext = None
         self.sqltext = None
         self.custom_sql = None
+        self.file_controls = None
+        self.file_queries = {}  # not None to be checkable right away
+        self.downloadcb = None
         super().__init__()
 
     def _setup_gui(self):
         super()._setup_gui()
         self._add_backend_controls()
         self._add_tables_controls()
+
+    def _load_tables_from_file(self):
+        if not self.path_to_res_file:
+            self.Error.empty_path()
+            return
+        self.Error.empty_path.clear()
+        try:
+            selects = self._parse_sql_resource_file(self.path_to_res_file)
+        except OSError:
+            self.Error.file_os_error()
+        else:
+            self.Error.file_os_error.clear()
+            self.file_queries.update(selects)
+            self.refresh_tables()
+
+    def _parse_sql_resource_file(self, filename):
+        """throws `OSError` - file may be unable to open"""
+        import re, os
+        # for simple queries this will work, assume that user can only SELECT from.
+        selects = {}  # name:query
+        # This order is precious:
+        name_pattern = re.compile("^-- name:(.*)$")
+        ignore_line = re.compile("^--.*")
+        start_query = re.compile("^SELECT|select")
+        stop_query = re.compile(";$");
+        # lambda to uni transformation
+        trn = lambda x: str(x).strip().upper()
+        def delete_comments(line):
+            return line[:line.find('--')]
+        with open(self.path_to_res_file, 'r', encoding='utf-8') as resfile:
+            name = None
+            query = []
+            while line := resfile.readline():
+                # Name as first line, will be overriden when line duplicate
+                if n := name_pattern.search(line):
+                    name = n.group(1)
+                # ignore comment lines
+                elif ignore_line.match(line):
+                    continue
+                # oneline query has start and stop in single line
+                elif start_query.search(line) and stop_query.search(line):
+                    selects[trn(name)] = line
+                    name = None
+                # another if-tree,
+                # start analyzing lines as query
+                if start_query.search(line) and name is not None:
+                    query.append(line)  # sets state of appending consecutive lines
+                elif stop_query.search(line) and name is not None:
+                    query.append(line)  # add last line
+                    # process query
+                    selects[trn(name)] = ' '.join(map(delete_comments, query))
+                    query = []  # reset state
+                    name = None
+                # query is not empty - means query is loading as consecutive lines
+                elif query:
+                    query.append(line)
+        return selects
 
     def _add_backend_controls(self):
         box = self.serverbox
@@ -107,39 +168,40 @@ class OWSql(OWBaseSql):
         backend = self.get_backend()
         self.selected_backend = backend.display_name if backend else None
 
-    def _add_tables_controls(self):
-        box = gui.vBox(self.controlArea, 'Data Selection')
-        form = QGridLayout()
-        radio_buttons = gui.radioButtons(
-            box, self, 'data_source', orientation=form,
-            callback=self.__on_data_source_changed)
-        radio_table = gui.appendRadioButton(
-            radio_buttons, 'Table:', addToLayout=False)
-        radio_custom_sql = gui.appendRadioButton(
-            radio_buttons, 'Custom SQL:', addToLayout=False)
+    def _add_file_controls(self, box):
+        self.file_controls = gui.vBox(box)
+        self.file_controls.setVisible(False)
+        mt = gui.hBox(self.file_controls)
+        le = gui.lineEdit(mt, self, 'path_to_res_file')
+        le.setToolTip('Path to file you load queries from. Be careful with choose.')
+        load = gui.button(self.file_controls, self, 'Load tables from file',
+                          callback=self._load_tables_from_file)
 
+    def _add_tables_controls(self):
+        vbox = gui.vBox(self.controlArea, "Tables")
+        box = gui.vBox(vbox)
         self.tables = TableModel()
 
-        self.tablecombo = ComboBoxSearch(
+        self.tablecombo = QComboBox(
             minimumContentsLength=35,
             sizeAdjustPolicy=QComboBox.AdjustToMinimumContentsLengthWithIcon
         )
         self.tablecombo.setModel(self.tables)
         self.tablecombo.setToolTip('table')
         self.tablecombo.activated[int].connect(self.select_table)
+        box.layout().addWidget(self.tablecombo)
 
-        self.tabletext = QLineEdit(placeholderText='TABLE_NAME')
-        self.tabletext.setToolTip('table')
-        self.tabletext.editingFinished.connect(self.select_table)
-        self.tabletext.setVisible(False)
+        self._add_file_controls(box)
 
         self.custom_sql = gui.vBox(box)
-        self.custom_sql.setVisible(self.data_source == self.CUSTOM_SQL)
+        self.custom_sql.setVisible(False)
         self.sqltext = QTextEdit(self.custom_sql)
         self.sqltext.setPlainText(self.sql)
         self.custom_sql.layout().addWidget(self.sqltext)
 
+
         mt = gui.hBox(self.custom_sql)
+
         cb = gui.checkBox(mt, self, 'materialize', 'Materialize to table ')
         cb.setToolTip('Save results of the query in a table')
         le = gui.lineEdit(mt, self, 'materialize_table_name')
@@ -147,18 +209,15 @@ class OWSql(OWBaseSql):
 
         gui.button(self.custom_sql, self, 'Execute', callback=self.open_table)
 
-        form.addWidget(radio_table, 1, 0, Qt.AlignLeft)
-        form.addWidget(self.tablecombo, 1, 1)
-        form.addWidget(self.tabletext, 1, 1)
-        form.addWidget(radio_custom_sql, 2, 0, Qt.AlignLeft)
+        box.layout().addWidget(self.custom_sql)
 
         gui.checkBox(box, self, "guess_values",
                      "Auto-discover categorical variables",
                      callback=self.open_table)
 
-    def __on_data_source_changed(self):
-        self.custom_sql.setVisible(self.data_source == self.CUSTOM_SQL)
-        self.select_table()
+        self.downloadcb = gui.checkBox(box, self, "download",
+                                       "Download data to local memory",
+                                       callback=self.open_table)
 
     def highlight_error(self, text=""):
         err = ['', 'QLineEdit {border: 2px solid red;}']
@@ -172,6 +231,14 @@ class OWSql(OWBaseSql):
         return self.backends[self.backendcombo.currentIndex()]
 
     def on_connection_success(self):
+        if getattr(self.backend, 'missing_extension', False):
+            self.Warning.missing_extension(
+                ", ".join(self.backend.missing_extension))
+            self.download = True
+            self.downloadcb.setEnabled(False)
+        if not is_postgres(self.backend):
+            self.download = True
+            self.downloadcb.setEnabled(False)
         super().on_connection_success()
         self.refresh_tables()
         self.select_table()
@@ -182,6 +249,8 @@ class OWSql(OWBaseSql):
 
     def clear(self):
         super().clear()
+        self.Warning.missing_extension.clear()
+        self.downloadcb.setEnabled(True)
         self.highlight_error()
         self.tablecombo.clear()
         self.tablecombo.repaint()
@@ -193,44 +262,52 @@ class OWSql(OWBaseSql):
             return
 
         self.tables.append("Select a table")
-        if self.backend.n_tables(self.schema) <= MAX_TABLES:
-            self.tables.extend(self.backend.list_tables(self.schema))
-            index = self.tablecombo.findText(str(self.table))
-            self.tablecombo.setCurrentIndex(index if index != -1 else 0)
-            self.tablecombo.setVisible(True)
-            self.tabletext.setVisible(False)
-        else:
-            self.tablecombo.setVisible(False)
-            self.tabletext.setVisible(True)
+        self.tables.append("Custom SQL")
+        self.file_controls.setVisible(True)
+        self.tables.extend(self.file_queries.keys())
+        self.tables.extend(self.backend.list_tables(self.schema))
+        index = self.tablecombo.findText(str(self.table))
+        self.tablecombo.setCurrentIndex(index if index != -1 else 0)
         self.tablecombo.repaint()
 
     # Called on tablecombo selection change:
     def select_table(self):
-        if self.data_source == self.TABLE:
+        curIdx = self.tablecombo.currentIndex()
+        table_name = self.tablecombo.itemText(curIdx)
+
+        if table_name != "Custom SQL" and not table_name in self.file_queries:
+            self.custom_sql.setVisible(False)
             return self.open_table()
         else:
+            self.custom_sql.setVisible(True)
             self.data_desc_table = None
-            if self.database_desc:
-                self.database_desc["Table"] = "(None)"
+            self.database_desc["Table"] = "(None)"
             self.table = None
-            if len(str(self.sql)) > 14:
-                return self.open_table()
+            if table_name in self.file_queries:
+                self.sqltext.setPlainText(self.file_queries[table_name])
+            # repaint custom_sql with loaded query
+            # ivokes custom query automatically or file query - disabled.
+            # if len(str(self.sql)) > 14:
+            #     return self.open_table()
         return None
 
+
     def get_table(self):
-        if self.backend is None:
-            return None
         curIdx = self.tablecombo.currentIndex()
-        if self.data_source == self.TABLE and curIdx <= 0 and \
-                self.tabletext.text() == "":
+        if curIdx <= 0:
             if self.database_desc:
                 self.database_desc["Table"] = "(None)"
             self.data_desc_table = None
             return None
 
-        if self.data_source == self.TABLE:
-            self.table = self.tables[curIdx] if curIdx > 0 else \
-                self.tabletext.text()
+        if self.tablecombo.itemText(curIdx) in self.file_queries:
+            self.table = self.tables[self.tablecombo.currentIndex()]
+            self.database_desc["Table"] = "Loaded from File"
+            # from textarea, because of possible modifications
+            # and sqltext is loaded from :meth:`select_table`
+            what = self.sql = self.sqltext.toPlainText()
+        elif self.tablecombo.itemText(curIdx) != "Custom SQL":
+            self.table = self.tables[self.tablecombo.currentIndex()]
             self.database_desc["Table"] = self.table
             if "Query" in self.database_desc:
                 del self.database_desc["Query"]
@@ -274,7 +351,7 @@ class OWSql(OWBaseSql):
 
         sample = False
 
-        if len(table) > LARGE_TABLE and self.guess_values:
+        if table.approx_len() > LARGE_TABLE and self.guess_values:
             confirm = QMessageBox(self)
             confirm.setIcon(QMessageBox.Warning)
             confirm.setText("Attribute discovery might take "
@@ -304,45 +381,45 @@ class OWSql(OWBaseSql):
             QApplication.restoreOverrideCursor()
             table.domain = domain
 
-        if len(table) > AUTO_DL_LIMIT:
-            if is_postgres(self.backend):
-                confirm = QMessageBox(self)
-                confirm.setIcon(QMessageBox.Warning)
-                confirm.setText("Data appears to be big. Do you really "
-                                "want to download it to local memory?\n"
-                                "Table length: {:,}. Limit {:,}".format(
-                    len(table), MAX_DL_LIMIT))
+        if self.download:
+            if table.approx_len() > AUTO_DL_LIMIT:
+                if is_postgres(self.backend):
+                    confirm = QMessageBox(self)
+                    confirm.setIcon(QMessageBox.Warning)
+                    confirm.setText("Data appears to be big. Do you really "
+                                    "want to download it to local memory?\n"
+                                    "Table length: {:,}. Limit {:,}".format(table.approx_len(), MAX_DL_LIMIT))
 
-                if len(table) <= MAX_DL_LIMIT:
-                    confirm.addButton("Yes", QMessageBox.YesRole)
-                no_button = confirm.addButton("No", QMessageBox.NoRole)
-                sample_button = confirm.addButton("Yes, a sample",
-                                                  QMessageBox.YesRole)
-                confirm.exec()
-                if confirm.clickedButton() == no_button:
-                    return None
-                elif confirm.clickedButton() == sample_button:
-                    table = table.sample_percentage(
-                        AUTO_DL_LIMIT / len(table) * 100)
-            else:
-                if len(table) > MAX_DL_LIMIT:
-                    QMessageBox.warning(
-                        self, 'Warning',
-                        "Data is too big to download.\n"
-                        "Table length: {:,}. Limit {:,}".format(len(table), MAX_DL_LIMIT)
-                    )
-                    return None
-                else:
-                    confirm = QMessageBox.question(
-                        self, 'Question',
-                        "Data appears to be big. Do you really "
-                        "want to download it to local memory?",
-                        QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-                    if confirm == QMessageBox.No:
+                    if table.approx_len() <= MAX_DL_LIMIT:
+                        confirm.addButton("Yes", QMessageBox.YesRole)
+                    no_button = confirm.addButton("No", QMessageBox.NoRole)
+                    sample_button = confirm.addButton("Yes, a sample",
+                                                      QMessageBox.YesRole)
+                    confirm.exec()
+                    if confirm.clickedButton() == no_button:
                         return None
+                    elif confirm.clickedButton() == sample_button:
+                        table = table.sample_percentage(
+                            AUTO_DL_LIMIT / table.approx_len() * 100)
+                else:
+                    if table.approx_len() > MAX_DL_LIMIT:
+                        QMessageBox.warning(
+                            self, 'Warning',
+                            "Data is too big to download.\n"
+                            "Table length: {:,}. Limit {:,}".format(table.approx_len(), MAX_DL_LIMIT)
+                        )
+                        return None
+                    else:
+                        confirm = QMessageBox.question(
+                            self, 'Question',
+                            "Data appears to be big. Do you really "
+                            "want to download it to local memory?",
+                            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+                        if confirm == QMessageBox.No:
+                            return None
 
-        table.download_data(MAX_DL_LIMIT)
-        table = Table(table)
+            table.download_data(MAX_DL_LIMIT)
+            table = Table(table)
 
         return table
 
